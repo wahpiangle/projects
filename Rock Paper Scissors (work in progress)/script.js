0db78750d886d44af1f02b1dad998eb7cc2e665f
wahpiangle <116425066+wahpiangle@users.noreply.github.com>
2022-11-19 16:36:57 +0800

    complete

--- Rock Paper Scissors (work in progress)/script.js
+++ Rock Paper Scissors (work in progress)/script.js
@@ -1,36 +1,65 @@
 let computerScore = 0;
+let computerScoreId = document.querySelector('#computer-score');
 let playerScore = 0;
+let playerScoreId = document.querySelector('#player-score');
 let roundWinner ='';
+let choicePlayerIcon = document.querySelector('#playerChoice');
+let choiceComputerIcon = document.querySelector('#computerChoice');
+let results = document.querySelector('#result-banner');
 
 //player selection
 const selectionBtns = document.querySelectorAll('button');
 selectionBtns.forEach(selectionBtns=> {
     selectionBtns.addEventListener('click', e=> {
         let playerChoice = selectionBtns.dataset.selection;
-        makeSelection(playerChoice)
+        if (playerChoice == "rock"){
+            choicePlayerIcon.textContent = '🌑'
+        }
+        
+        else if(playerChoice == "scissors"){
+            choicePlayerIcon.textContent = '✂'
+        }
+
+        else if(playerChoice == "paper"){
+            choicePlayerIcon.textContent = '📄'
+        }
+        playGame(getComputerChoice(), playerChoice);
+
+        if(playerScoreId.textContent == 5 || computerScoreId.textContent == 5){
+            
+            if(computerScoreId.textContent == 5){
+                results.textContent = "You've lost!";
+            }
+            else if(playerScoreId.textContent == 5){
+                results.textContent = "You've won!";
+            }
+            else{results.textContent = "";}
+            computerScoreId.textContent = 0;
+            playerScoreId.textContent = 0;
+        }
     })
 })
 
-function makeSelection(selection) {
-    console.log(selection);
-}
 
 function getComputerChoice(){
     let computerChoice = Math.floor(Math.random()*3)+1; //rng will be 1,2 or 3
     switch(computerChoice){
         case 1:
+            choiceComputerIcon.textContent = '🌑'
             return "rock";
-            break;
         case 2:
+            choiceComputerIcon.textContent = '📄'
             return "paper";
-            break;
         case 3:
+            choiceComputerIcon.textContent = '✂'
             return "scissors";
-            break;
     }
 }
 
+
 function playGame(choiceOfComputer, choiceOfPlayer){
+    results.textContent= "";
+
     if (choiceOfComputer == choiceOfPlayer)
     {
         roundWinner = "tie";
@@ -39,17 +68,17 @@ function playGame(choiceOfComputer, choiceOfPlayer){
             (choiceOfComputer == "paper" && choiceOfPlayer == "rock") ||
             (choiceOfComputer == "scissors" && choiceOfPlayer == "paper"))
     {
-        computerScore++;
+        computerScoreId.textContent ++;
         roundWinner = 'computer';
     }
-    else{
-        playerScore++;
+
+    else if ( (choiceOfComputer == "paper" && choiceOfPlayer =="scissors")||
+            (choiceOfComputer == "scissors" && choiceOfPlayer == "rock") ||
+            (choiceOfComputer == "rock" && choiceOfPlayer == "paper"))
+    {
+        playerScoreId.textContent++;
         roundWinner='player';
     }
-}
-
-function isGameOver(){
-    return playerScore == 5 || computerScore == 5;
 }
 
 
