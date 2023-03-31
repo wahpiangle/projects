c88ff49ff56d07db3776776606a63e6a2d8cd35d
wahpiangle <116425066+wahpiangle@users.noreply.github.com>
2023-03-31 17:10:18 +0800

    update

--- Shopping Cart/src/components/Cart/Cart.jsx
+++ Shopping Cart/src/components/Cart/Cart.jsx
@@ -3,24 +3,37 @@ import { useSelector, useDispatch } from 'react-redux';
 import { RxCross1 } from 'react-icons/rx';
 import { incrementItem, decrementItem, clearItem } from '../../redux/cartSlice';
 
-export default function Cart(){
+export default function Cart() {
     const cart = useSelector(state => state.cart.value);
     const dispatch = useDispatch();
 
-    console.log(cart)
-    function handleIncrementItem(id){
+    function handleIncrementItem(id) {
         dispatch(incrementItem(id));
     }
 
-    function handleDecrementItem(id){
+    function handleDecrementItem(id) {
         dispatch(decrementItem(id));
     }
 
-    function handleClearItem(id){
+    function handleClearItem(id) {
         dispatch(clearItem(id));
     }
+    // const handleSubmit = async (event) => {
+    //     event.preventDefault();
+    //     const response = await fetch('/create-checkout-session', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify({
+    //             cartTotalPrice: cart.totalPrice
+    //         })
+    //     });
+    //     const session = await response.json();
+    //     window.location = session.url;
+    // };
 
-    return(
+    return (
         <div className="cart">
             <div className="cart-title">Cart</div>
             <div className='cart-container'>
@@ -28,7 +41,7 @@ export default function Cart(){
                     {cart.itemsInCart.map(item => (
                         <div className='cart-item' key={item.id}>
                             <div className='cart-item-image'>
-                                <img src={item.image[0].url}/>
+                                <img src={item.image[0].url} />
                             </div>
                             <div className='cart-item-details'>
                                 <div className='cart-item-details-name'>{item.name}</div>
@@ -36,10 +49,10 @@ export default function Cart(){
                             </div>
                             <div className='cart-item-quantity'>
                                 <div className='cart-item-quantity-minus' onClick={() => handleDecrementItem(item.id)}>-</div>
-                                    {item.quantity}
+                                {item.quantity}
                                 <div className='cart-item-quantity-plus' onClick={() => handleIncrementItem(item.id)}>+</div>
                             </div>
-                            <RxCross1 className='cart-clear-item' onClick={()=> handleClearItem(item.id)}></RxCross1>
+                            <RxCross1 className='cart-clear-item' onClick={() => handleClearItem(item.id)}></RxCross1>
                         </div>
                     ))}
                 </div>
@@ -51,7 +64,10 @@ export default function Cart(){
                         <div className='cart-total-subtitle'>Shipping</div>
                         <div className='cart-total-shipping'>No Shipping Options are available</div>
                     </div>
-                    <div className='cart-total-checkout'>Proceed to Checkout</div>
+                    <input type="hidden" name="cartTotalPrice" value={cart.totalPrice} />
+                    <div className='button-wrapper'>
+                        <a href="https://buy.stripe.com/test_9AQ3ehcER27o5689AA"><button className='cart-total-checkout'>Proceed to Checkout</button></a>
+                    </div>
                 </div>
             </div>
         </div>
